72340a03c4edd1d0cabbe38d32cc463d06438102
Kohalahal <52988648+kohalahal@users.noreply.github.com>
2020-06-30 19:19:05 +0900

    06.30.19.19

--- src/main/java/org/kitchen/login/SecurityConfig.java
+++ src/main/java/org/kitchen/login/SecurityConfig.java
@@ -3,13 +3,12 @@ package org.kitchen.login;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication
                                                         .builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web
                                                     .configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web
-                                                    .servlet.configuration.EnableWebMvcSecurity;
  
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
  
     @Override
