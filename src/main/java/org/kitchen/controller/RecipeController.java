7063f8b2ddb1647ad780cfac48aff008d6df7416
Kohalahal <52988648+kohalahal@users.noreply.github.com>
2020-07-10 10:24:34 +0900

    07101024

--- src/main/java/org/kitchen/controller/RecipeController.java
+++ src/main/java/org/kitchen/controller/RecipeController.java
@@ -1,27 +1,19 @@
 package org.kitchen.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-
-import org.kitchen.domain.CategoryVO;
 import javax.servlet.http.HttpSession;
 
 import org.kitchen.domain.ContentVO;
-import org.kitchen.domain.Criteria;
 import org.kitchen.domain.RecipeVO;
-import org.kitchen.domain.UserVO;
 import org.kitchen.service.RecipeService;
 import org.kitchen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.extern.log4j.Log4j;
@@ -36,36 +28,12 @@ public class RecipeController {
 	@Autowired
 	private UserService userService;
 
-	@GetMapping("/register")
-	public void register() {
-
-	}
-
-	@PostMapping("/register")
-	public String register(RecipeVO recipe, ContentVO content, RedirectAttributes rttr) {
-		log.info("register : " + recipe);
-
-//		recipeService.register(recipe);
-//		recipeService.registerCon(content);
-		recipeService.register(recipe, content);
-
-		rttr.addFlashAttribute("result", recipe.getRno());
-		rttr.addFlashAttribute("resultCon", content.getRno());
-
-		return "redirect:/recipe/list";
-	}
-
 	@GetMapping("/registration")
 	public void registerform(Model model) {
 		model.addAttribute("recipe", new RecipeVO());
 	}
-	
-	@GetMapping("/registration2")
-	public void register2form(Model model) {
-		model.addAttribute("recipe", new RecipeVO());
-	}
 
-	@PostMapping("/registrationTest")
+	@PostMapping("/result")
 	public @ModelAttribute("recipe") RecipeVO register2save(@ModelAttribute("recipe") RecipeVO recipe) {
 		// recipeService에 저장하기
 		recipeService.register(recipe);
